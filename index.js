51ca7c121ec539c8a063efe5c98ea8990fa13bfc
Lorenzo Giorgini <lorenzo.giorgini@outlook.com>
2021-09-16 22:45:25 +0200

    commit

--- index.js
+++ index.js
@@ -1,51 +1,85 @@
 let getBook = () => {
-    return fetch("https://striveschool-api.herokuapp.com/books" , { 
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        let row = document.querySelector(".row")
+  fetch("https://striveschool-api.herokuapp.com/books", {})
+    .then((response) => response.json())
+    .then((data) => {
+      let row = document.querySelector(".row");
 
-        data.forEach(element => {
-            let col = document.createElement("div")
-            col.classList = ("col-md-4")
-            col.innerHTML =  `
-                <div class="card" style="width: 200px; height: 350px;">
+      data.forEach((element , index) => {
+        let col = document.createElement("div");
+        col.classList = "col-md-4";
+        col.innerHTML = `
+                <div class="card" style="margin-bottom: 50px">
                     <img src="${element.img}" class="card-img-top" style="width: 200px; height: 250px;">
                     <div class="card-body">
-                    <h5 class="card-title"></h5>
-                    <p class="card-text"></p>
-                    <button class="btn btn-primary" onclick="addToCart(event)">Add to cart</button>
+                    <p class="card-title">${element.title}</p>
+                    <button class="btn btn-primary" onclick="addToCart(${index})">Add to cart</button>
+                    <button class="btn btn-secondary" onclick="hide(event)">Skip</button>
                     </div>
                 </div>
-            ` 
-                
-            row.appendChild(col)
-        });
-    }) 
+            `;
+
+        row.appendChild(col);
+        array.push(element)
+        elements.push(col)
+      });
+    });
+};
+
+let array = [];
+let elements = [];
+
+let addToCart = (i) => {
+  let savedCard = document.createElement("li");
+  array.push(savedCard);
+  let h6 = document.createElement("h6")
+  h6.innerHTML = `<h6 class="card-title" style="color: black;">${array[i].title}</h6>`
+  savedCard.appendChild(h6)
+  let ul = document.querySelector(".list-group");
+  ul.appendChild(savedCard);
+};
+
+let hide = (event) => {
+  event.target.closest(".col-md-4").remove()
 }
 
-let array = []
-
-let addToCart = (e) => {
-    e.target.closest(".col-md-4").remove()
-    let savedCard = e.target.closest(".col-md-4")
-    array.push(savedCard)
-    savedCard.innerHTML = `
-        <div class="card" style="width: 200px; height: 350px;">
-            <img src="${element.img}" class="card-img-top" style="width: 200px; height: 250px;">
-            <div class="card-body">
-            <h5 class="card-title"></h5>
-            <button class="btn btn-primary" onclick="skip(event)">Skip</button>
-            </div>
-        </div>
-    `
-    console.log(savedCard)
-    let ul = document.querySelector("#div-list")
-    ul.appendChild(savedCard)
+let search = () => {
+  let input = document.querySelector("input[type=search]").value
+  if (input.length > 2) {
+    console.log(elements)
+    elements.forEach((element , index) => {
+      element.remove()
+    })
+    elements.splice(0 , elements.length)
+    console.log(elements)
+    let tmp = []
+    fetch("https://striveschool-api.herokuapp.com/books", {})
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((element , index) => {
+        let col = document.createElement("div");
+        col.classList = "col-md-4";
+        col.innerHTML = `
+                <div class="card" style="margin-bottom: 50px">
+                    <img src="${element.img}" class="card-img-top" style="width: 200px; height: 250px;">
+                    <div class="card-body">
+                    <p class="card-title">${element.title}</p>
+                    <button class="btn btn-primary" onclick="addToCart(${index})">Add to cart</button>
+                    <button class="btn btn-secondary" onclick="hide(event)">Skip</button>
+                    </div>
+                </div>
+            `;
+        tmp.push(col)
+        elements.push(col)
+      });
+      tmp = tmp.filter(name => name.children[0].children[1].firstElementChild.innerText.toLowerCase().includes(input.toLowerCase()))
+      let row = document.querySelector(".row");
+      tmp.forEach((elem) => {
+        row.appendChild(elem)
+      })
+    });
+  }
 }
-
 
 window.onload = () => {
-    getBook()
-}
+  getBook();
+};
